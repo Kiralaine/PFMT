9cfa87f035f6bbe570f48181e4f69e3c18499224
Kiralaine <k9034449@gmail.com>
2025-10-22 22:59:26 +0500

    default init

--- src/PFMT.AppHost/AppHost.cs
+++ src/PFMT.AppHost/AppHost.cs
@@ -1,5 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.PFMT_Server>("pfmt-server");
+var webApp = builder.AddProject<Projects.PFMT_Server>("pfmt-server");
+var mysql = builder.AddMySql("myDatabase").WithLifetime(ContainerLifetime.Persistent); ;
+
+// This is the magic line that links the DB to the app
+webApp.WithReference(mysql);
 
 builder.Build().Run();
---
+++ src/PFMT.Infastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFMT.Infastructure.Persistence.Configurations
+{
+    public class UserConfiguration 
+    {
+    }
+}
